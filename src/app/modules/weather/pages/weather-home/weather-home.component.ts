6e69c69bfe952a0fb310e77de35851e889fb1439
Danilo Silva <danilo.silva@msn.com>
2024-12-26 17:56:04 -0300

    adiciona componente WeatherHome e configura rotas

---
+++ src/app/modules/weather/pages/weather-home/weather-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-home',
+  templateUrl: './weather-home.component.html',
+  styles: [],
+})
+export class WeatherHomeComponent {}
